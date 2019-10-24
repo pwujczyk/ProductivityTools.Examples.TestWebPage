eb2dbbc6b8282109c296f0ec57f5edd9494535dc
pwujczyk <pwujczyk@ecovadis.com>
2019-10-24 20:50:36 +0200

    initial commit

---
+++ ProductivityTools.Examples.TestWebPage/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductivityTools.Examples.TestWebPage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DateController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
